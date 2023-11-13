a213cc08fcb482abfa7ba733f08756055ba18d4f
Redinkt <Redinkto>
2023-11-13 12:03:22 +0300

    nats subscribe

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -5,6 +5,9 @@ import (
 	"main/internal/config"
 	"main/internal/storage/postgres"
 	"os"
+
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/stan.go"
 )
 
 func main() {
@@ -18,6 +21,22 @@ func main() {
 		fmt.Println("Database connected")
 	}
 	_=storage
+
+
+	nc, err := nats.Connect("nats://localhost:4222")
+    if err != nil {
+        fmt.Println(err.Error())
+    }
+
+    defer nc.Close()
+	sc, err := stan.Connect("test-cluster", "client-1", stan.NatsConn(nc))
+
+
+	sub, _ := sc.Subscribe("my-channel", func(msg *stan.Msg) {
+		//msg
+	})
+    defer sub.Unsubscribe()
+
 
 	//router
 
